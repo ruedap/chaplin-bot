e63b14f7bb06a7b533bebba2c3a6232076def841
ruedap <ruedap@ruedap.com>
2012-07-09 22:10:25 +0900

    Sinatra version 1.0

---
+++ Rakefile
@@ -0,0 +1,5 @@
+require './tweet.rb'
+
+task :cron do
+  Tweet.new.daily_tweet
+end
---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'sinatra'
+require './tweet.rb'
+
+get '/' do
+  'under construction'
+end
+
+get '/random_tweet' do
+#  Tweet.new.random_tweet  # 動作チェックが終わったらコメントアウトすること
+end
