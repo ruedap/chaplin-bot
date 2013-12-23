eae1f2b0d48ea834ef4cf79527a0159c1b3fef36
ruedap <ruedap@ruedap.com>
2013-12-23 22:14:51 +0900

    Refactor

--- spec/models/remark_spec.rb
+++ spec/models/remark_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Remark do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.count' do
+    it 'returns 31' do
+      expect(Remark.count).to eq(31)
+    end
+  end
+
+  describe '.first' do
+    it 'returns first phrase' do
+      expect(Remark.first.phrase).to eq('私の最高傑作は次回作だ。')
+    end
+  end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Tweet do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.reset' do
+    before { described_class.reset(Remark.all) }
+
+    it 'returns 31' do
+      expect(described_class.all.count).to eq(31)
+    end
+  end
+
+  describe '.empty?' do
+    context 'with before reset' do
+      it 'returns true' do
+        expect(described_class.empty?).to be_true
+      end
+    end
+
+    context 'with after reset' do
+      before { described_class.reset(Remark.all) }
+
+      it 'returns false' do
+        expect(described_class.empty?).to be_false
+      end
+    end
+  end
 end
