d9fea9bc63615bbbd460ba5a840f534c2b80839e
ruedap <ruedap@ruedap.com>
2020-08-02 01:00:59 +0900

    refactor: Refactor tweet() for production

--- src/index.debug.ts
+++
@@ -1,5 +0,0 @@
-import { getTodaysText } from "./text";
-import { tweet } from "./tweet";
-
-const t = getTodaysText(new Date());
-tweet(`${t.text} [debug:${Date.now()}]`);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { getTodaysText } from "./text";
-import { tweet } from "./tweet";
+import { twitter, options, tweet } from "./tweet";
 
-const t = getTodaysText(new Date());
-tweet(t.text);
+const args = process.argv.slice(2);
+const isProduction = args.some((arg) => arg === "--production");
+const isDebug = !isProduction && args.some((arg) => arg === "--debug");
+
+const t = twitter(options(isProduction));
+let text = getTodaysText(new Date()).text;
+text = isDebug ? `${text} [debug:${Date.now()}]` : text;
+tweet(t, text);
