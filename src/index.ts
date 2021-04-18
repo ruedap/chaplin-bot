7c9015425c3ededb8605e4c8898596b5eec237b9
ruedap <ruedap@ruedap.com>
2021-04-18 12:51:32 +0900

    chore: Update debug code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { getTodaysText } from "./text";
 import { getToday } from "./date";
 import { twitter, options, tweet } from "./tweet";
+import { Temporal } from "proposal-temporal";
 
 const args = process.argv.slice(2);
 const isProduction = args.some((arg) => arg === "--production");
@@ -8,6 +9,6 @@ const isDebug = !isProduction && args.some((arg) => arg === "--debug");
 
 const t = twitter(options(isProduction));
 let text = getTodaysText(getToday()).text;
-text = isDebug ? `${text} [debug:${Date.now()}]` : text;
+text = isDebug ? `${text} [debug:${Temporal.now.instant()}]` : text;
 
 tweet(t, text);
